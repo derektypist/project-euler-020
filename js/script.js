60dd359ec4f663f1c0e585ca7f5edcd6b10e62d5
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-05-28 10:11:30 +0000

    Added bigIntFactorial, sumFactorialDigits and clearInfo at script.js

--- js/script.js
+++ js/script.js
@@ -15,3 +15,27 @@ function getNumberInfo() {
     // Display Information in the Browser
     document.getElementById("numinfo").innerHTML = txt;
 }
+
+/*
+     Function to Find Factorial of a given number n
+     bigIntFactorial(1) returns 1
+     bigIntFactorial(2) returns 2
+*/
+function bigIntFactorial(n) {
+    return n<=1 ? BigInt(1) : BigInt(n) * bigIntFactorial(n-1);
+}
+
+/*
+    Function to return the sum of the digits in the factorial of n
+    sumFactorialDigits(10)  returns 27
+    sumFactorialDigits(100) returns 648
+*/
+function sumFactorialDigits(n) {
+    return bigIntFactorial(n).toString().split('').reduce((a,b) => a + Number(b),0);
+}
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById("numinfo") = txt;
+}
